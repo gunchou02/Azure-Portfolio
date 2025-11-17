5743af283ba612d5aad8890185eb6ebcbef47a72
binbin <gunchou02@gmail.com>
2025-11-17 13:13:27 +0900

    New

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Azure デプロイテスト</title>
+
+    <style>
+
+        body { 
+
+            font-family: "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif; 
+
+            text-align: center; 
+
+            padding-top: 50px; 
+
+        }
+
+        h1 { color: #0078D4; }
+
+    </style>
+
+</head>
+
+<body>
+
+    <h1>Azure デプロイ成功！</h1>
+
+    <p>このページが表示されている場合、Webサーバーは正常に稼働しています。</p>
+
+    <p>現在のPHPバージョン: <?php echo phpversion(); ?></p>
+
+</body>
+
+</html>
